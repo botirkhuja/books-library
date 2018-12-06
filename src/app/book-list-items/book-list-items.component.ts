79cb945b0c6423324c6cb912c03fb8cd4fc88a0e
Djakhangir <J.atahanov@yahoo.com>
2018-12-05 22:31:58 -0500

    tables are ready

--- src/app/book-list-items/book-list-items.component.ts
+++ src/app/book-list-items/book-list-items.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookListItemsComponent implements OnInit {
 
+  public listOfUsers = [
+     {
+    id: 1,
+    name:'One',
+    author: 'One to One',
+    totalAmount:5,
+    dates:'Tue Nov 13 2018 16:33:06 GMT-0600 (Eastern Time)',
+    category: 'math',
+    
+     }, 
+     {
+    id: 2,
+    name:'Two',
+    author: 'Someone',
+    totalAmount:5,
+    dates:'Tue Nov 13 2018 16:33:06 GMT-0600 (Eastern Time)',
+    category: 'romance',
+     },
+     {
+    id: 3,
+    name:'Three',
+    author: 'Anyone',
+    totalAmount:5,
+    dates:'Tue Nov 13 2018 16:33:06 GMT-0600 (Eastern Time)',
+    category: 'history',
+      }
+ ]
+
   constructor() { }
 
   ngOnInit() {
